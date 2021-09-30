16adc07864ea7d1c2ceaeaa5831b1b43a3d95a5b
Huchan1124 <garlic1124@gmail.com>
2021-09-30 17:31:37 +0800

    Update 2021-09-30T09:31:33.212Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,20 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+//AOS
+AOS.init(); // swiper
+
+var swiper = new Swiper(".userResponseSwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false
+  },
+  controller: {
+    inverse: true
+  }
 });
 //# sourceMappingURL=all.js.map
