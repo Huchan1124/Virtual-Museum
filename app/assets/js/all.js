fface59b976c26ca998b61a7959f53cf07cd1263
Huchan1124 <garlic1124@gmail.com>
2021-09-30 17:31:14 +0800

    套入swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,22 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+//AOS
+AOS.init();
+
+
+// swiper
+const swiper = new Swiper(".userResponseSwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  controller: {
+    inverse: true,
+  },
+  
+
 });
+
